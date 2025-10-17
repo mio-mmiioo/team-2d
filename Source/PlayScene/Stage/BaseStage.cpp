e03195846bba875e49b36a952928db3e4391bb2e
mio-mmiioo <240672@jc-21.jp>
2025-10-17 19:25:35 +0900

    ステージのデータをセットする関数を作成 Stage.cppを削除

--- Source/PlayScene/Stage/BaseStage.cpp
+++ Source/PlayScene/Stage/BaseStage.cpp
@@ -2,35 +2,21 @@
 #include <assert.h>
 #include "../../../Library/CsvReader.h"
 #include "../Player/Player.h"
+#include "Stage.h"
 
+namespace BASESTAGE {
+	char filename[64]; // 生成するcurrentStage_のファイル名
+	const int LINE_POS_LEFT = 256; // currentStageを描画し始める位置を設定
+}
 
 BaseStage::BaseStage()
 {
-	CsvReader* csv = new CsvReader("data/stage/baseStage.csv");
-	for (int line = 0; line < csv->GetLines(); line++) {
-		std::vector<int> mapLine;
-		for (int column = 0; column < csv->GetColumns(line); column++) {
-			int c = csv->GetInt(line, column);
-			mapLine.push_back(c);
-		}
-		baseStage_.push_back(mapLine);
-	}
-	delete csv;
-
 	hImage_ = LoadGraph("data/image/parts.png");
 	assert(hImage_ > 0);
 	imageSize_ = VECTOR2(64, 64);
 
-	for (int y = 0; y < baseStage_.size(); y++) {
-		for (int x = 0; x < baseStage_[y].size(); x++) {
-			int c = baseStage_[y][x];
-			if (c == 1) {
-				int px = x * imageSize_.x + imageSize_.x / 2.0f;
-				int py = y * imageSize_.y + imageSize_.y / 2.0f;
-				new Player(VECTOR2(px, py));
-			}
-		}
-	}
+	SetStageData(&baseStage_, "data/stage/baseStage.csv");
+	CreateStage(1); // 最初のステージを生成
 }
 
 BaseStage::~BaseStage()
@@ -53,6 +39,46 @@ void BaseStage::Draw()
 			}
 		}
 	}
+
+	for (int y = 0; y < currentStage_.size(); y++) {
+		for (int x = 0; x < currentStage_[y].size(); x++) {
+			int c = currentStage_[y][x];
+			if (c == 2) {
+				DrawRectGraph(x * w + BASESTAGE::LINE_POS_LEFT, y * h, 3 * w, 1 * h, w, h, hImage_, TRUE);
+			}
+		}
+	}
+}
+
+void BaseStage::SetStageData(std::vector<std::vector<int>> *stage, const char* filename)
+{
+	CsvReader* csv = new CsvReader(filename);
+	for (int line = 0; line < csv->GetLines(); line++) {
+		std::vector<int> mapLine;
+		for (int column = 0; column < csv->GetColumns(line); column++) {
+			int c = csv->GetInt(line, column);
+			mapLine.push_back(c);
+		}
+		stage->push_back(mapLine);
+	}
+	delete csv;
+
+	for (int y = 0; y < stage->size(); y++) {
+		for (int x = 0; x < (*stage)[y].size(); x++) {
+			int c = (*stage)[y][x];
+			if (c == 1) {
+				int px = x * imageSize_.x + imageSize_.x / 2.0f;
+				int py = y * imageSize_.y + imageSize_.y / 2.0f;
+				new Player(VECTOR2(px, py));
+			}
+		}
+	}
+}
+
+void BaseStage::CreateStage(int number)
+{
+	sprintf_s<64>(BASESTAGE::filename, "data/stage/stage%02d.csv", number);
+	SetStageData(&currentStage_, BASESTAGE::filename);
 }
 
 void BaseStage::ChooseStage()
--- Source/PlayScene/Stage/BaseStage.h
+++ Source/PlayScene/Stage/BaseStage.h
@@ -10,9 +10,12 @@ public:
 	void Draw() override;
 
 private:
+	void SetStageData(std::vector<std::vector<int>> *stage, const char* filename); // ステージデータをセットするための関数
+	void CreateStage(int number);
 	void ChooseStage(); // ランダムな値を入手して次のステージを生成するための変数
 
 	std::vector<std::vector<int>> baseStage_; // ステージの両端の部分
+	std::vector<std::vector<int>> currentStage_; // 現在のステージ
 
 	int hImage_;
 };
