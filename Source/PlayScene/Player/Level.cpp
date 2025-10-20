51cde65983c2993bc6b60218330dee58af449dc1
mio-mmiioo <240672@jc-21.jp>
2025-10-20 23:11:37 +0900

    レベルアップのサウンドのプログラム書こうとしたけど、思った通りにならなかった

--- Source/PlayScene/Player/Level.cpp
+++ Source/PlayScene/Player/Level.cpp
@@ -1,30 +1,62 @@
 #include "Level.h"
+#include "DxLib.h"
 #include <vector>
+#include "../../../Library/Time.h"
+#include "../../Sound.h"
 
 namespace Level {
 	std::vector<int> count_ = { 0, 7,15,23,32,41,51,61,72,83,94 };
 	std::vector<float> level_ = { 9.0f,8.0f,7.58f,7.20f,6.86f,6.55f,6.26f,6.00f,5.76f,5.54f };
 
-	float CountToTime(int count)
+	float timer_ = 0.0f; // 次の音を鳴らすまでのタイマー
+	int counter_ = 0; // 現在の音が何番目か
+	const float TIME = 1.0f;
+	const int COUNT = 3;
+}
+
+void Level::Update()
+{
+	if (Level::timer_ > 0)
 	{
-		for (int i = count_.size() - 1; i >= 0; i--)
+		Level::timer_ -= Time::DeltaTime();
+		if (timer_ <= 0)
 		{
-			if (count >= count_[i])
+			counter_ += 1;
+			PlaySoundMem(Sound::scale[counter_], DX_PLAYTYPE_BACK, TRUE);
+			
+			if (counter_ < Level::COUNT)
 			{
-				return level_[i];
+				timer_ = timer_ + Level::TIME;
 			}
 		}
 	}
-	float RateCount(int count)
+}
+
+float Level::CountToTime(int count)
+{
+	for (int i = Level::count_.size() - 1; i >= 0; i--)
 	{
-		float rate;
-		for (int i = count_.size() - 1; i >= 0; i--)
+		if (count >= Level::count_[i])
 		{
-			if (count >= count_[i])
-			{
-				rate = ((float)count - (float)count_[i]) / ((float)count_[i + 1] - (float)count_[i]) * 100;
-				return rate;
-			}
+			return Level::level_[i];
 		}
 	}
+}
+
+float Level::RateCount(int count)
+{
+	float rate;
+	for (int i = Level::count_.size() - 1; i >= 0; i--)
+	{
+		if (count >= Level::count_[i])
+		{
+			rate = ((float)count - (float)Level::count_[i]) / ((float)Level::count_[i + 1] - (float)Level::count_[i]) * 100;
+			return rate;
+		}
+	}
+}
+
+void Level::SoundUp()
+{
+	timer_ = Level::TIME;
 }
--- Source/PlayScene/Player/Level.h
+++ Source/PlayScene/Player/Level.h
@@ -1,6 +1,9 @@
 #pragma once
 
 namespace Level {
+	void Update();
 	float CountToTime(int count); // カウントから音階の時間を求める
 	float RateCount(int count); // カウントからレベルアップまでのパーセントを求める
+
+	void SoundUp(); // レベルアップする音源
 }
